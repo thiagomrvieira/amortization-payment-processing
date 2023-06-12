767242e26411841a6a5bf50ae22578dc61a632b6
Thiago Vieira <tmaciel@digital-works.com>
2023-06-12 21:15:46 +0100

    fix: Add pending field

--- app/Http/Resources/AmortizationResource.php
+++ app/Http/Resources/AmortizationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,7 +17,9 @@ class AmortizationResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'schedule_date' => $this->schedule_date,
+            'schedule_date' => Carbon::parse($this->schedule_date)->format('d-m-Y'),
+            'paid_at' => $this->state === 'paid' ? Carbon::parse($this->schedule_date)->diffForHumans() : null,
+            'state' => $this->state,
             'amount' => number_format($this->amount,2,",","."),
             'project' => new ProjectResource($this->project),
         ];
