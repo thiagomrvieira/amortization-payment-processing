978c7341352d1d5aff5e8cd6a95b6ff1507bdbd2
Thiago Vieira <tmaciel@digital-works.com>
2023-06-15 19:48:58 +0100

    fix: Rename blade view and fix web routes

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,7 @@
 use App\Http\Controllers\Api\V1\AmortizationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 Route::prefix('v1')->group(function () {
     Route::get('/amortizations', [AmortizationController::class, 'index']);
 });
